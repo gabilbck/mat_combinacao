87eea9898267b43a99bb3d85766e551df513e936
gadulb <gabrielieduardalembeck@gmail.com>
2023-06-05 20:56:28 -0300

    add

--- paginas/capitulo1.php
+++ paginas/capitulo1.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../src/css/style.css">
+    <link rel="stylesheet" href="../src/css/style1.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Patrick+Hand+SC&display=swap" rel="stylesheet">
@@ -18,10 +18,20 @@
     <hr class="barra-branca">
     <br>
     <form id="combinacoes" action="resultado1.php" method="get">
-        <div class="">
+            <div class="carta">
+                <div style="grid-template-columns: 1fr;">
+                    <i>Caro cavaleiro,
+                    <br><br>Venho a pedido do Rei Josias, do Reino de Inforny, para lhe trazer uma mensagem. Escute-me pois lhe digo: o reino corre grande perigo! Estamos sendo atacados por monstros e criaturas misticas. Dragões e bruxas com suas feitiçarias. Precisamos que saia em uma jornada para derrotar o mal que nos assombra, tome cuidado e saiba andar no caminho da luz e da sombra. Mas tome muito cuidado, apenas há uma maneira correta de chegar até o outro lado. Cada decisão importa, então preste atenção se não quiser que sua vida acabe. Escolha sabiamente o mapa, ou então se meterá em silada.
+                    <br><br>Ass. Reino de Inforny.</i><br><br>
+                </div>
+            </div>
             <center>
-                <h3>Primeira missão: escolher o mapa certo</h3>
                 <br>
+                <hr class="barra-branca-meio">
+                <br><br>
+                <h3>Primeira missão: escolher o mapa certo</h3>
+                <b>(1 mapa necessário para prosseguir)</b>
+                <br><br><br>
             </center>
             <!-- ITENS -->
             <div class="container col-4">
@@ -48,31 +58,29 @@
             </div>
             <br>
             <center><button style="color: #8b5742;" type="button" onclick="verificar()">COMEÇAR A VIAGEM!</button></center>
-        </div>
     </form>
     <br><br>
     <hr class="barra-branca">
     <div class="footer">
         <h4>Informações do projeto</h4>
-                <a style="font-size: 14px; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;" target="blank" href="https://github.com/gadulb/joguinho">GitHub</a>
-
+        <a style="font-size: 14px; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;" target="blank" href="https://github.com/gadulb/joguinho">GitHub</a>
     </div>
 
-<script>
-    let verificar = () => {
-        const selectionados = document.querySelectorAll('input[type="checkbox"]:checked').length;
-        console.log(selectionados); 
-        const formulario = document.getElementById("combinacoes");
-        if (selectionados == 1) {
-            formulario.submit(); 
-        } else {
-            if (selectionados < 1){
-                alert("Erro: você selecionou MENOS de 1 elementos!");
-            } else if (selectionados > 1) {
-                alert("Erro: você selecionou MAIS de 1 elementos!");
+    <script>
+        let verificar = () => {
+            const selectionados = document.querySelectorAll('input[type="checkbox"]:checked').length;
+            console.log(selectionados); 
+            const formulario = document.getElementById("combinacoes");
+            if (selectionados == 1) {
+                formulario.submit(); 
+            } else {
+                if (selectionados < 1){
+                    alert("Erro: você selecionou MENOS de 1 elementos!");
+                } else if (selectionados > 1) {
+                    alert("Erro: você selecionou MAIS de 1 elementos!");
+                }
             }
-        }
-    };
-</script>
+        };
+    </script>
 </body>
 </html>
--- paginas/resultado1.php
+++ paginas/resultado1.php
@@ -39,7 +39,7 @@ if ($mapa3 == 'op'){ // Variáveis que têm o value="cer"
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="../src/css/style.css" type="text/css" rel="stylesheet">
+    <link href="../src/css/style1.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Patrick+Hand+SC&display=swap" rel="stylesheet">
@@ -47,35 +47,35 @@ if ($mapa3 == 'op'){ // Variáveis que têm o value="cer"
 </head>
 <body>
     <div class="resultado">
-    <div class="header">
-        <h1 style="font-family: 'Patrick Hand SC', cursive;">Horizontes Sombrios: O Caminho do Exilado</h1>
-    </div>
-    <hr class="barra-branca">
-    <br>
-    <?php // Se o usuário venceu
-    if (isset($msgVenceu)){
-        echo '<div class="texto-resultado">';
-        echo '<div class="resposta"';
-        echo $msgVenceu;
-        echo '</div>';
-        echo '<br><center><button><a href="capitulo2.php">PRÓXIMO CAPÍTULO</a></button></center>';
-        echo '</div>';
-    } else{ // Se o usuário perdeu
-        echo '<div class="texto-resultado">';
-        echo '<div class="resposta"';
-        echo $msgPerdeu;
-        echo $mapaErr;
-        echo $msgPerdeu1;
-        echo '</div>';
-        echo '<br><center><button><a href="capitulo1.php">VOLTAR AO INÍCIO</a></button></center>';
-        echo '</div>';
-    }
-    ?>
-    <br><br>
-    <hr class="barra-branca">
-    <div class="footer">
-        <h4>Informações do projeto</h4>
-        <a style="font-size: 14px; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;" target="blank" href="https://github.com/gadulb/joguinho">GitHub</a>
-    </div>
+        <div class="header">
+            <h1 style="font-family: 'Patrick Hand SC', cursive;">Horizontes Sombrios: O Caminho do Exilado</h1>
+        </div>
+        <hr class="barra-branca">
+        <br>
+        <?php // Se o usuário venceu
+        if (isset($msgVenceu)){
+            echo '<div class="texto-resultado">';
+            echo '<div class="resposta"';
+            echo $msgVenceu;
+            echo '</div>';
+            echo '<br><center><button><a href="capitulo2.php">PRÓXIMO CAPÍTULO</a></button></center>';
+            echo '</div>';
+        } else{ // Se o usuário perdeu
+            echo '<div class="texto-resultado">';
+            echo '<div class="resposta"';
+            echo $msgPerdeu;
+            echo $mapaErr;
+            echo $msgPerdeu1;
+            echo '</div>';
+            echo '<br><center><button><a href="capitulo1.php">VOLTAR AO INÍCIO</a></button></center>';
+            echo '</div>';
+        }
+        ?>
+        <br><br>
+        <hr class="barra-branca">
+        <div class="footer">
+            <h4>Informações do projeto</h4>
+            <a style="font-size: 14px; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;" target="blank" href="https://github.com/gadulb/joguinho">GitHub</a>
+        </div>
     </div>
 </body>
