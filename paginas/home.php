9d6f7f2165187d23a11d282b04af06a21b4a60b9
gadulb <gabrielieduardalembeck@gmail.com>
2023-06-05 21:53:20 -0300

    add

--- paginas/home.php
+++ paginas/home.php
@@ -17,8 +17,12 @@
     </div>
     <hr class="barra-branca">
     <br>
-    <div class="texto-intro">
-        <p>Caro cavalheiro, <br><br>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+    <div class="carta">
+        <div style="grid-template-columns: 1fr;">
+            <i>Caro cavaleiro,
+            <br><br>Venho a pedido do Rei Josias, do Reino de Inforny, para lhe trazer uma mensagem. Escute-me pois lhe digo: o reino corre grande perigo! Estamos sendo atacados por monstros e criaturas misticas. Dragões e bruxas com suas feitiçarias. Precisamos que saia em uma jornada para derrotar o mal que nos assombra, tome cuidado e saiba andar no caminho da luz e da sombra. Mas tome muito cuidado, cada decisão importa, então preste atenção se não quiser que sua vida acabe antes mesmo de começar. 
+            <br><br>Boa sorte, cavaleiro!</i><br><br>
+        </div>
     </div>
     <button><a href="capitulo1.php">INICIAR SUA JORNADA</a></button>
     <br><br>
